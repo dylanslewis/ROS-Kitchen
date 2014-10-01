e89f4e5582bbc7600f2a9abe7ee5170018bb309b
Your Name Comes Here <dylanslewis@me.com>
2014-10-01 16:22:48 +0100

    Initial Commit

---
+++ Kitchen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kitchen
+//
+//  Created by Dylan Lewis on 01/10/2014.
+//  Copyright (c) 2014 Dylan Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kitchen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kitchen
+//
+//  Created by Dylan Lewis on 01/10/2014.
+//  Copyright (c) 2014 Dylan Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
